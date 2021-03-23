c617aab9921458dd7b9e0ea5c6ba04a2786f0b43
Kaitlyn Yancey <kyancey423@gmail.com>
2021-03-22 23:18:25 -0400

    adding chart features

--- src/journalpage/journal.js
+++ src/journalpage/journal.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import JournalContext from '../JournalContext';
 import JournalEntry from './journal-entry';
+import { Line, Pie } from 'react-chartjs-2';
+
 const { API_BASE_URL } = require('../config')
+
 
 class JournalPage extends Component {
     constructor(props) {
@@ -9,10 +12,13 @@ class JournalPage extends Component {
         this.state = {
             entries: [],
             month: 'all',
-            currentUser: 'ADMIN'
+            currentUser: 'ADMIN',
+            stressLevels: '',
+            dates: '',
+            moods: [],
         }
     }
-    
+
     static contextType = JournalContext;
 
 
@@ -20,50 +26,82 @@ class JournalPage extends Component {
         const month = e.target.value
         var filteredEntries = this.context.entries.filter(entry =>
             entry.userid === this.context.currentUser)
-        if(month !== 'all') {
+        if (month !== 'all') {
             filteredEntries = filteredEntries.filter(entry =>
                 entry.month_created === month
-              )
+            )
         }
+        const stressLevels = filteredEntries.map(entry =>
+            entry.stress_level
+        )
+        const dates = filteredEntries.map(entry =>
+            entry.date_created)
+        const moods = filteredEntries.map(entry =>
+            entry.mood)
         this.setState({
-            entries: filteredEntries
+            entries: filteredEntries,
+            stressLevels: stressLevels,
+            dates: dates,
+            moods: moods,
         })
     }
 
     handleDeleteEntry = entryId => {
         const newEntries = this.state.entries.filter(entry =>
             entry.id !== entryId
-          )
-          this.setState({
-            entries: newEntries
-          })
+        )
+        const stressLevels = newEntries.map(entry =>
+            entry.stress_level
+        )
+        const dates = newEntries.map(entry =>
+            entry.date_created)
+        const moods = newEntries.map(entry =>
+                entry.mood)
+
+        this.setState({
+            entries: newEntries,
+            stressLevels: stressLevels,
+            dates: dates,
+            moods:moods,
+        })
     }
     componentDidMount() {
         fetch(`${API_BASE_URL}/entries`, {
             method: 'GET',
             headers: {
-              'content-type': 'application/json',
-              'Authorization': `Bearer ${process.env.REACT_APP_API_KEY}`,
+                'content-type': 'application/json',
+                'Authorization': `Bearer ${process.env.REACT_APP_API_KEY}`,
             }
-          })
-          .then(res => {
-            if (!res.ok) {
-              return res.json().then(error => {
-                throw error
-              })
-            }
-            return res.json()
-          })
-          .then(response => {
-            const userEntries = response.filter(entry => 
-                entry.userid === this.context.currentUser)
-            this.setState({
-              entries: userEntries
+        })
+            .then(res => {
+                if (!res.ok) {
+                    return res.json().then(error => {
+                        throw error
+                    })
+                }
+                return res.json()
             })
-          })
-          .catch(error => {
-            this.setState({ error })
-          })
+            .then(response => {
+                const userEntries = response.filter(entry =>
+                    entry.userid === this.context.currentUser)
+                const stressLevels = userEntries.map(entry =>
+                    entry.stress_level
+                )
+                const dates = userEntries.map(entry =>
+                    entry.date_created)
+                const moods = userEntries.map(entry =>
+                    entry.mood)
+
+                this.setState({
+                    entries: userEntries,
+                    stressLevels: stressLevels,
+                    dates: dates,
+                    moods: moods,
+                })
+            })
+            .catch(error => {
+                this.setState({ error })
+            })
         // const userEntries = this.context.entries.filter(entry =>
         //     entry.userid === this.context.currentUser) 
         // this.setState({
@@ -73,6 +111,7 @@ class JournalPage extends Component {
 
     render() {
         const entries = this.state.entries
+        
         return (
             <div className="logpage">
                 <section className="center">
@@ -95,6 +134,82 @@ class JournalPage extends Component {
                         <option value="December">Dec</option>
                     </select>
                 </section>
+                <div className="wrapper">
+                    <Pie
+                        data={{
+                            labels: ['Excited', 'Happy', 'Bored', 'Sad', 'Nervous', 'Angry'],
+                            datasets: [
+                                {
+                                    label: 'Mood',
+                                    backgroundColor: [
+                                        '#B21F00',
+                                        '#C9DE00',
+                                        '#2FDE00',
+                                        '#00A6B4',
+                                        '#6800B4'
+                                    ],
+                                    hoverBackgroundColor: [
+                                        '#501800',
+                                        '#4B5000',
+                                        '#175000',
+                                        '#003350',
+                                        '#35014F'
+                                    ],
+                                    data: [
+                                        this.state.moods.filter(i => i === "Excited").length,
+                                        this.state.moods.filter(i => i === "Happy").length,
+                                        this.state.moods.filter(i => i === "Bored").length,
+                                        this.state.moods.filter(i => i === "Sad").length,
+                                        this.state.moods.filter(i => i === "Nervous").length,
+                                        this.state.moods.filter(i => i === "Angry").length
+                                    ]
+                                }
+                            ]
+                        }}
+                        options={{
+                            title: {
+                                display: true,
+                                text: 'Mood Tracker',
+                                fontSize: 20
+                            },
+                            legend: {
+                                display: true,
+                                position: 'right'
+                            }
+                        }}
+
+                    />
+                </div>
+                <br></br>
+                <div className="wrapper">
+                    <Line
+                        data={{
+                            labels: this.state.dates,
+                            datasets: [
+                                {
+                                    label: 'Stress Level',
+                                    fill: false,
+                                    lineTension: 0.5,
+                                    backgroundColor: 'rgba(75,192,192,1)',
+                                    borderColor: 'rgba(0,0,0,1)',
+                                    borderWidth: 2,
+                                    data: this.state.stressLevels
+                                }
+                            ]
+                        }}
+                        options={{
+                            title: {
+                                display: true,
+                                text: 'Stress Level Tracker',
+                                fontSize: 20
+                            },
+                            legend: {
+                                display: false,
+                                position: 'right'
+                            }
+                        }}
+                    />
+                </div>
                 <section>
                     <ul className="entry-list">
                         {entries.map(entry =>
